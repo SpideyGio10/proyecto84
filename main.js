f637dadde204c039b4e36bfc04142cd3e0413828
SpideyGio10 <104469485+SpideyGio10@users.noreply.github.com>
2022-06-24 12:13:44 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+// Crea una referencia para el elemento canvas
+canvas=document.getElementById('myCanvas');
+ctx=canvas.getContext("2d");
+
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image(); //define una variable para una imagen nueva
+	img_imgTag.onload = uploadimg; // establece una función para cargar la variable
+	img_imgTag.src = img_image;   // carga la imagen
+}
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+//Escribe el código para obtener el evento key-pressed
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90)){
+		//Escribe el código para verificar el tipo de tecla que se presion
+		aplhabetkey();
+		document.getElementById("d1").innerHTML="Tu presionaste una tecla de letra"}
+	else {
+		otherkey();
+		document.getElementById("d1").innerHTML="tu presionaste otro simbolo"
+		console.log("tecla de alfabeto")
+	}
+}
+
+function aplhabetkey()
+{
+	//Sube la imagen con el mensaje.
+	img_image="Alpkey.png";
+	add(); 
+}
+function numberkey()
+{
+	img_image="numkey.png";
+	add();
+}
+function arrowkey()
+{
+	img_image="Arrkey.png";
+	add();
+}
+function specialkey()
+{
+	img_image="spkey.png";
+	add();
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
